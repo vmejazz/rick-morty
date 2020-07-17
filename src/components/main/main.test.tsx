df40a6db7b2a521bd5c27f4edc05468914607695
vmejazz <vmejazz@gmail.com>
2020-07-17 12:59:15 +0300

    Renew shapshots

--- src/components/main/main.test.tsx
+++ src/components/main/main.test.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Main from "./main";
-jest.mock(
-  "../list-of-characters/list-of-characters.tsx",
-  () => "ListOfCharacters"
-);
+
+jest.mock("../list-of-characters/list-of-characters.tsx", () => {
+  return {
+    __esModule: true,
+    A: true,
+    default: () => {
+      return <ul></ul>;
+    },
+  };
+});
 
 test("Render Main component", () => {
   const tree = render(<Main />);
